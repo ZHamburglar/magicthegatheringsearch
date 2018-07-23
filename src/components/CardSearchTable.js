6e900cfecf32ceb797338bd7485d026b21bd2fdc
ZHamburglar <kollinbrandenburg@gmail.com>
2018-07-22 22:43:35 -0500

    removing extraneous code

--- src/components/CardSearchTable.js
+++ src/components/CardSearchTable.js
@@ -13,36 +13,11 @@ import "react-table/react-table.css";
 
 import "./index.css";
 
-const range = len => {
-  const arr = [];
-  for (let i = 0; i < len; i++) {
-    arr.push(i);
-  }
-  return arr;
-};
-
-const newPerson = () => {
-  return {
-    firstName: namor.generate({ words: 2, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 })
-  };
-};
-
-export function makeData(len = 555) {
-    console.log("making data")
-  return range(len).map(d => {
-    return {
-      ...newPerson(),
-      children: range(10).map(newPerson)
-    };
-  });
-}
 
 class CardSearchTable extends Component {
   constructor() {
     super();
     this.state = {
-      data: makeData()
     };
   }
 
